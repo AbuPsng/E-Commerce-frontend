2dc9e836c9c7a7bfa552943735f4635beec8a6fe
Phassang <abupsngdscz@gmail.com>
2024-09-19 07:19:30 +0530

    remove description field from product creation component

--- src/pages/admin/management/newproduct.tsx
+++ src/pages/admin/management/newproduct.tsx
@@ -18,7 +18,6 @@ const NewProduct = () => {
   const [category, setCategory] = useState<string>("");
   const [price, setPrice] = useState<number>(1000);
   const [stock, setStock] = useState<number>(1);
-  const [description, setDescription] = useState<string>("");
 
   const [newProduct] = useNewProductMutation();
   const navigate = useNavigate();
@@ -36,7 +35,6 @@ const NewProduct = () => {
       const formData = new FormData();
 
       formData.set("name", name);
-      formData.set("description", description);
       formData.set("price", price.toString());
       formData.set("stock", stock.toString());
 
@@ -71,16 +69,6 @@ const NewProduct = () => {
                 placeholder="Name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-
-            <div>
-              <label>Description</label>
-              <textarea
-                required
-                placeholder="Description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
               />
             </div>
 
